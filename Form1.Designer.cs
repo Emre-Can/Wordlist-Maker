72ac98cd32977bdf5c3fbe81ce64b978fd5f66a1
Emre Can ÇİMKE <emrecancimke@gmail.com>
2022-06-27 23:35:22 +0300

    first

---
+++ Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace WordListMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnOlustur = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(128, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(373, 25);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(5, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Dosya Yolu :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(14, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 19);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Dosya Adı :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox2.Location = new System.Drawing.Point(128, 31);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(373, 25);
+            this.textBox2.TabIndex = 2;
+            // 
+            // btnOlustur
+            // 
+            this.btnOlustur.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnOlustur.Location = new System.Drawing.Point(383, 70);
+            this.btnOlustur.Name = "btnOlustur";
+            this.btnOlustur.Size = new System.Drawing.Size(118, 31);
+            this.btnOlustur.TabIndex = 6;
+            this.btnOlustur.Text = "Oluştur";
+            this.btnOlustur.UseVisualStyleBackColor = true;
+            this.btnOlustur.Click += new System.EventHandler(this.btnOlustur_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numericUpDown1.Location = new System.Drawing.Point(94, 56);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.numericUpDown2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 56);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(221, 85);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Karakter";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 19);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "En Az :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 19);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "En Çok :";
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Consolas", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.numericUpDown2.Location = new System.Drawing.Point(94, 25);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 25);
+            this.numericUpDown2.TabIndex = 10;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(504, 144);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnOlustur);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Word List Maker";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnOlustur;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WordListMaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        String[] karakterler = new String[] { "a", "b", "c", "ç", "d", "e", "f", "g", "ğ", "h", "ı", "i", "j", "k", "l", "m", "n", "o", "ö", "p", "r", "s", "ş", "t", "u", "ü", "v", "w", "q", "x", "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+        String kelime;
+       
+
+        private void btnOlustur_Click(object sender, EventArgs e)
+        {
+            int karakteruz = karakterler.Length;
+            String yol=textBox1.Text+"\\"+textBox2.Text+".txt";
+            FileStream fs = new FileStream(yol, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+            textBox1.Enabled = false;
+            textBox2.Enabled = false;
+            btnOlustur.Enabled = false;
+            numericUpDown1.Enabled = false;
+            numericUpDown2.Enabled = false;
+            if (numericUpDown2.Value < 2)
+            {
+                for (int i = 0; i < karakteruz; i++)
+                {
+                    sw.WriteLine(karakterler[i]);
+                    sw.Flush();
+                }
+            }
+
+            if (numericUpDown1.Value > 1 && numericUpDown2.Value<3)
+            {
+                for (int i = 0; i < karakteruz; i++)
+                {
+                    for (int j = 0; j < karakteruz; j++)
+                    {
+                        kelime = karakterler[i] + karakterler[j];
+                        sw.WriteLine(kelime);
+                        sw.Flush();
+                    }
+                }
+            }
+            if (numericUpDown1.Value > 2 && numericUpDown2.Value<4)
+            {
+                for (int i = 0; i < karakteruz; i++)
+                {
+                    for (int j = 0; j < karakteruz; j++)
+                    {
+                        for (int k = 0; k < karakteruz; k++)
+                        {
+                            kelime = karakterler[i] + karakterler[j] + karakterler[k];
+                            sw.WriteLine(kelime);
+                            sw.Flush();
+                        }
+                    }
+                }
+            }
+            if (numericUpDown1.Value > 3 && numericUpDown2.Value < 5)
+            {
+                for (int i = 0; i < karakteruz; i++)
+                {
+                    for (int j = 0; j < karakteruz; j++)
+                    {
+                        for (int k = 0; k < karakteruz; k++)
+                        {
+                            for (int l = 0; l < karakteruz; l++)
+                            {
+                                kelime = karakterler[i] + karakterler[j] + karakterler[k] + karakterler[l];
+                                sw.WriteLine(kelime);
+                                sw.Flush();
+                            }
+                        }
+                    }
+                }
+            }
+            if (numericUpDown1.Value > 4 && numericUpDown2.Value < 6)
+            {
+                for (int i = 0; i < karakteruz; i++)
+                {
+                    for (int j = 0; j < karakteruz; j++)
+                    {
+                        for (int k = 0; k < karakteruz; k++)
+                        {
+                            for (int l = 0; l < karakteruz; l++)
+                            {
+                                for (int m = 0; m < karakteruz; m++)
+                                {
+                                    kelime = karakterler[i] + karakterler[j] + karakterler[k] + karakterler[l] + karakterler[m];
+                                    sw.WriteLine(kelime);
+                                    sw.Flush();
+                                }
+
+                            }
+                        }
+                    }
+                }
+            }
+            if (numericUpDown1.Value > 5 && numericUpDown2.Value < 7)
+            {
+                for (int i = 0; i < karakteruz; i++)
+                {
+                    for (int j = 0; j < karakteruz; j++)
+                    {
+                        for (int k = 0; k < karakteruz; k++)
+                        {
+                            for (int l = 0; l < karakteruz; l++)
+                            {
+                                for (int m = 0; m < karakteruz; m++)
+                                {
+                                    for (int s = 0; s < karakteruz; s++)
+                                    {
+                                        kelime = karakterler[i] + karakterler[j] + karakterler[k] + karakterler[l] + karakterler[m] + karakterler[s];
+                                        sw.WriteLine(kelime);
+                                        sw.Flush();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            sw.Close();
+            fs.Close();
+            MessageBox.Show("Word List Oluşturma Başarılı");
+            textBox1.Enabled = true;
+            textBox2.Enabled = true;
+            btnOlustur.Enabled = true;
+            numericUpDown1.Enabled = true;
+            numericUpDown2.Enabled = true;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.Minimum = numericUpDown2.Value;
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            numericUpDown2.Maximum = numericUpDown1.Value;
+        }
+    }
+}
